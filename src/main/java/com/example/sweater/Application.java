eed84e7c4fe80df0d9163d6f7a74a3cfa033e5e0
Smartrb111 <100877090+Smartrb111@users.noreply.github.com>
2022-08-03 19:48:47 +0300

    WrbApp

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,17 @@
+package com.example.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@PropertySource({"classpath:application.properties"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
