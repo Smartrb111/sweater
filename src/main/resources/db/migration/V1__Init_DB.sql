974beb38e87b23f9d44d214843d5b251bc5458f0
ibaev <ivan_baev@relex.ru>
2022-08-10 14:16:01 +0300

    fix поправил миграции и сущности

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -1,12 +1,10 @@
-
-
 create table message (
-                         id int8 not null,
+                         id bigserial
+                             constraint pk_message primary key,
                          filename varchar(255),
                          tag varchar(255),
                          text varchar(2048) not null,
-                         user_id int8,
-                         primary key (id)
+                         user_id int8
 );
 
 create table user_role (
@@ -15,13 +13,13 @@ create table user_role (
 );
 
 create table usr (
-                     id int8 not null,
+                     id int8
+                        constraint pk_usr primary key,
                      activation_code varchar(255),
                      active boolean not null,
                      email varchar(255),
                      password varchar(255) not null,
-                     username varchar(255) not null,
-                     primary key (id)
+                     username varchar(255) not null
 );
 
 alter table if exists message
